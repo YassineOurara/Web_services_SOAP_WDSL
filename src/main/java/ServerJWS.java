11afd23e1b17a5c6417170cabc94ccebe801ed11
YassineOurara <yassineourara@gmail.com>
2023-03-28 11:00:26 +0000

    1st commit

---
+++ src/main/java/ServerJWS.java
@@ -0,0 +1,10 @@
+import jakarta.xml.ws.Endpoint;
+
+import  org.sid.BanqueService;
+public class ServerJWS {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
+        System.out.println("Web service déployé sur http://0.0.0.0:9191/");
+    }
+
+}
