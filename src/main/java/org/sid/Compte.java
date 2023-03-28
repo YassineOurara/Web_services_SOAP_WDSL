3bb6b7a47aace125992f43968bc42aea91b82d20
YassineOurara <yassineourara@gmail.com>
2023-03-28 11:40:32 +0000

    2nd commit

--- src/main/java/org/sid/Compte.java
+++ src/main/java/org/sid/Compte.java
@@ -1,10 +1,20 @@
 package org.sid;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+
+
+
 import java.util.Date;
+@XmlRootElement(name = "compte")
+@XmlAccessorType(XmlAccessType.FIELD)
 
 public class Compte {
     private  int code;
     private double solde;
+    @XmlTransient
     private Date dateCreation;
 
     public int getCode() {
